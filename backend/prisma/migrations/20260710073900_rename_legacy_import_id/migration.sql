02ad87006f0233c61e5744fbd242c7750e5b541d
Himangshu <himangshukumardas75695@gmail.com>
2026-07-10 13:22:49 +0530

    chore: remove manual migration file to scrub legacy term

--- backend/prisma/migrations/20260710073900_rename_legacy_import_id/migration.sql
+++
@@ -1,8 +0,0 @@
--- RenameOwlcoderIdToLegacyImportId
--- Renames the owlcoderId column to legacyImportId in Question and HubArticle tables
-
--- Question table
-ALTER TABLE `Question` RENAME COLUMN `owlcoderId` TO `legacyImportId`;
-
--- HubArticle table
-ALTER TABLE `HubArticle` RENAME COLUMN `owlcoderId` TO `legacyImportId`;
