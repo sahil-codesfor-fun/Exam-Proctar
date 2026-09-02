5e5a4c9cf06d3fa4b54eab62b2517bd132be850d
sahil-codesfor-fun <sahilbhardwaj4488@gmail.com>
2026-09-02 14:43:39 +0530

    Prisma query updates to make it faster

--- frontend/dist/assets/Teachers-BA-dYEOJ.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./vendor-react-core-FBF55MVh.js";import{T as t}from"./TeachersList-BolsjLoI.js";import"./index-LZhvPb9Q.js";import"./vendor-export-tools-CjmKG9zm.js";import"./vendor-icons-ZVJgj3U2.js";const m=()=>r.jsx(t,{apiEndpoint:"/superadmin/teachers",isSuperAdmin:!0});export{m as default};
